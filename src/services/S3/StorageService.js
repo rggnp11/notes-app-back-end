aedc96476dc2c3c7eca877b4eff96bce18894180
h2yhm <heri2000@yahoo.com>
2023-07-26 22:40:00 +0700

    Add dash to filename to upload to S3

--- src/services/S3/StorageService.js
+++ src/services/S3/StorageService.js
@@ -8,7 +8,7 @@ class StorageService {
   writeFile(file, meta) {
     const parameter = {
       Bucket: process.env.AWS_BUCKET_NAME, // Nama S3 Bucket yang digunakan
-      Key: +new Date() + meta.filename, // Nama berkas yang akan disimpan
+      Key: +new Date() + '-' + meta.filename, // Nama berkas yang akan disimpan
       Body: file._data, // Berkas (dalam bentuk Buffer) yang akan disimpan
       ContentType: meta.headers['content-type'], // MIME Type berkas yang akan disimpan
     };
